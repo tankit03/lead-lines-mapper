2261ef919797fa3d1149cfb2ec4a070e3a038e17
Tanish Hupare <huparetanish@gmail.com>
2025-07-18 23:30:59 -0700

    UI styling for navbar, signup, and login page

---
+++ server/tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./views/**/*.ejs"],
+   theme: {
+     extend: {
+      fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        }
+     },
+   },
+   plugins: [],
+ }
